11d3231ec94b2929bd06d5ba9565716f79b22901
Jose Angel <jangel.romero@gmail.com>
2017-01-16 23:45:20 +0100

    Mejoras

--- src/Slider/Slider_Flickity/scripts.js
+++ src/Slider/Slider_Flickity/scripts.js
@@ -9,10 +9,12 @@ var $carousel = $("#main_slider .carousel").flickity({
 var $caption_title = $("#main_slider .caption-title");
 var $caption_descr = $("#main_slider .caption-description");
 
-// $(document).ready( function() {
-//   //-------------------------
-//   $carousel.on( "select.flickity", function() {
-//     $caption_title.animateCss("fadeInUp");
-//     $caption_descr.animateCss("fadeInUp");
-//   });
-// });
+/*
+$(document).ready( function() {
+  //-------------------------
+  $carousel.on("select.flickity", function() {
+    $caption_title.animateCss("fadeInUp");
+    $caption_descr.animateCss("fadeInUp");
+  });
+});
+*/
